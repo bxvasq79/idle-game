8d5256bb6352f2e3febb400984c0fda2c9e3522b
bryan vasquez sierra <bxvasq79@harrisonburg.k12.va.us>
2019-09-17 14:26:17 -0400

    Update game.js

--- game.js
+++ game.js
@@ -5,10 +5,9 @@ postToPage(score); // Print to the page
 createButton('Button', btnPress); // Make a button
 changeTitle('Clicker Game');
 
-setInterval(btnPress, 1000);
+setInterval(btnPress, 600);
 
 function btnPress() {
   score++;
   postToPage(score);
 }
-
